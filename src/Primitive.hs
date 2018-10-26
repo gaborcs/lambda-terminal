deb23df02fbb299fc1e5beda6fece67ddf99ebab
Gabor Csizmadia <gaborcsiz@gmail.com>
2018-10-27 00:09:09 +0200

    remove ifZero primitive, it can be implemented using pattern matching

--- src/Primitive.hs
+++ src/Primitive.hs
@@ -4,31 +4,25 @@ import qualified Type as T
 import qualified Value as V
 
 data Primitive
-    = IfZero
-    | Plus
+    = Plus
     | Minus
     | Times
     deriving (Eq, Read, Show, Bounded, Enum)
 
 getDisplayName :: Primitive -> String
 getDisplayName p = case p of
-    IfZero -> "ifZero"
     Plus -> "+"
     Minus -> "-"
     Times -> "*"
 
 getType :: Primitive -> T.Type t
 getType p = case p of
-    IfZero -> T.Fn T.Int $ T.Fn (T.Var 0) $ T.Fn (T.Var 0) (T.Var 0)
     Plus -> binaryIntOpType T.Int
     Minus -> binaryIntOpType T.Int
     Times -> binaryIntOpType T.Int
 
 getValue :: Primitive -> V.Value c
 getValue p = case p of
-    IfZero -> V.Fn $ \maybeN -> Just $ V.Fn $ \maybeThen -> Just $ V.Fn $ \maybeElse -> case maybeN of
-        Just (V.Int 0) -> maybeThen
-        _ -> maybeElse
     Plus -> binaryIntOpValue $ \a b -> V.Int (a + b)
     Minus -> binaryIntOpValue $ \a b -> V.Int (a - b)
     Times -> binaryIntOpValue $ \a b -> V.Int (a * b)
