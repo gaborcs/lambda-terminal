270f0833dffda4539851586768c6edb9d84cc854
Gabor Csizmadia <gaborcsiz@gmail.com>
2019-01-18 20:03:23 +0100

    minor simplifications

--- src/Eval.hs
+++ src/Eval.hs
@@ -14,7 +14,7 @@ eval = eval' Map.empty
 eval' :: (Eq c, Ord d) => V.Env c -> Map.Map d (E.Expr d c) -> E.Expr d c -> Maybe (V.Value c)
 eval' env defs expr = case expr of
     E.Hole -> Nothing
-    E.Def defId -> Map.lookup defId defs >>= eval' env defs
+    E.Def key -> Map.lookup key defs >>= eval defs
     E.Var var -> join $ Map.lookup var env
     E.Fn alts -> Just $ V.Fn $ evalPatternMatching $ NonEmpty.toList alts
     E.Call callee arg -> do
@@ -28,19 +28,18 @@ eval' env defs expr = case expr of
     E.Integer n -> Just $ V.Integer n
     E.Primitive p -> Just $ getValue p
     where
-        evalPatternMatching alts maybeArgVal = case alts of
-            [] -> Nothing
-            (firstAltPatt, firstAltExpr):restOfAlts -> case match maybeArgVal firstAltPatt of
-                Just envExtension -> eval' (Map.union envExtension env) defs firstAltExpr
-                Nothing -> evalPatternMatching restOfAlts maybeArgVal
+        evalPatternMatching alts maybeArgVal = foldr f Nothing alts where
+            f (altPatt, altExpr) acc = case match maybeArgVal altPatt of
+                Just envExtension -> eval' (Map.union envExtension env) defs altExpr
+                Nothing -> acc
 
 match :: Eq c => Maybe (V.Value c) -> P.Pattern c -> Maybe (Map.Map E.VarName (Maybe (V.Value c)))
 match maybeValue patt = case patt of
     P.Wildcard -> Just Map.empty
     P.Var var -> Just $ Map.singleton var maybeValue -- maybeValue is not evaluated (yet) in this case
     P.Constructor patternConstructorKey patterns -> case maybeValue of
-        Just (V.Constructor valueConstructorKey values) ->
-            if patternConstructorKey == valueConstructorKey then mconcat <$> zipWithM match values patterns else Nothing
+        Just (V.Constructor valueConstructorKey values) | patternConstructorKey == valueConstructorKey ->
+            mconcat <$> zipWithM match values patterns
         _ -> Nothing
     P.Integer n -> case maybeValue of
         Just (V.Integer m) | n == m -> Just Map.empty
